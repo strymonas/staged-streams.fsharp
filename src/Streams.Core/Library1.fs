c63e06ed61421046d1a598efec00ac7efc5d0da2
Nick Palladinos <nick.palladinos@gmail.com>
2017-01-19 20:06:09 +0200

    Init structure

---
+++ src/Streams.Core/Library1.fs
@@ -0,0 +1,26 @@
+﻿namespace Streams.Core
+
+open FSharp.Quotations
+
+
+type CardType = AtMost1 | Many
+and ProducerShape<'T, 'S> =
+  | For    of ProducerFor<'T, 'S>
+  | Unfold of ProducerUnfold<'T, 'S>
+
+ and ProducerFor<'T, 'S> =
+    { upb :   'S -> Expr<int>;
+      index : 'S -> Expr<int> -> ('T -> Expr<unit>) -> Expr<unit> }
+ and ProducerUnfold<'T, 'S> =
+    { term : 'S -> Expr<bool>;
+      card : CardType;
+      step : 'S -> ('T -> Expr<unit>) -> Expr<unit> }
+
+and Init<'S> = 
+    abstract member Invoke<'R> : ('S -> Expr<'R>) -> Expr<'R>
+
+and StreamShape<'T> = interface end
+and Stream<'T> = StreamShape<Expr<'T>>
+
+
+//and Producer<'T>()
